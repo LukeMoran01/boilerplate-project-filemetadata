8bfedf609597cb03f01e1b131f26182ceee5d2e9
Replit user <>
2023-04-02 23:31:50 +0100

    feat: Use multer to get uploaded file metadata
    Use multer to get file metadeta in the post request and return some of it in JSON

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer')
 require('dotenv').config()
 
 var app = express();
+const upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -10,6 +12,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  let file = req.file;
+  console.log(file)
+  return res.json({
+    "name": file.originalname,
+    "type": file.mimetype,
+    "size": file.size
+  })
+})
 
 
 
